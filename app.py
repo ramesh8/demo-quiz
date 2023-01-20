8698ec0713243cef028bd73e1f8deecf17ff929e
Raja Ramesh Haddubangi <h.rajaramesh@gmail.com>
2023-01-20 16:20:05 +0530

    server side logic

--- app.py
+++ app.py
@@ -1,14 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+
+from flask_pymongo import PyMongo
+
+import datetime
+
+
 
 app = Flask(__name__)
+
+app.config['MONGO_URI'] = "mongodb://localhost:27017/practice"
+
+mongo = PyMongo(app)
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/quiz")
+@app.route("/quiz", methods=['GET'])
 def quiz():
     return render_template("quiz.html")
+
+@app.route("/quiz", methods=['POST'])
+def quiz_submit():
+    questions = request.json
+    
+    score = 0
+    for question in questions:
+        if "attempted" in question:
+            attemptedOption = question["options"][question["attemptedAnswer"]]
+            if attemptedOption == question["correctAnswer"]:
+                score += 1
+
+    userid = 1
+    quiz = {
+        "score":score,
+        "timestamp": datetime.datetime.now(),
+        "userid":userid
+    }
+    mongo.db.quiz.insert_one(quiz)
+    mongo.db.questions.insert_many(questions)
+
+
+    return jsonify({"score":score})
 
 #get questions from api, mongodb,...
 
